d297b09c657eb32a3decd8f15131795549a7b59b
Peleka <ego.iriska@gmail.com>
2022-02-12 20:11:32 +0300

    section Services

--- src/components/Main/Services/Card/Card.js
+++ src/components/Main/Services/Card/Card.js
@@ -2,15 +2,17 @@ import React from "react";
 import moduleClasses from './Card.module.scss'
 
 
-export const Card = ({photo, title, text, subtext, onClick, itemId}) => {
+export const Card = ({photo, title, text}) => {
 
-    return <div className={moduleClasses["card"]}
-             role={"button"}
-        >
-            <img className={moduleClasses["card__image"]} src={photo}/>
-            <h2 className={moduleClasses["card__title"]} >{title}</h2>
-            <p className={moduleClasses["card__text"]} >{text}</p>
-            <span className={moduleClasses["card__subtext"]} >{subtext}</span>
+    return (
+        <div className={moduleClasses["card"]}>
+            <div className={moduleClasses["card__image"]}>
+                <img src={photo}/>
+            </div>
+
+            <h2 className={moduleClasses["card__title"]}>{title}</h2>
+            <p className={moduleClasses["card__text"]}>{text}</p>
+            <span className={moduleClasses["card__subtext"]}>Подробнее</span>
         </div>
-
+    )
 }
--- src/components/Main/Services/Services.js
+++ src/components/Main/Services/Services.js
@@ -1,57 +1,60 @@
 import React from "react";
 import moduleClasses from './Services.module.scss'
 import globalClasses from "../../../App.module.scss";
-import project_photo from "../../../images/proekt_doma.png"
+import project1 from "../../../images/proekt_doma.png"
+import project2 from "../../../images/photo.png"
+import project3 from "../../../images/photo (1).png"
+import project4 from "../../../images/04.png"
 import {v1} from "uuid";
 import {Card} from "./Card/Card";
-import HorizontalScroll from "react-scroll-horizontal";
 
 export const Services = () => {
-    const cardsList = [
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-        {id: v1(), image: project_photo, title: "Общее обследование", text: "Общее обследование", subtext: "Подробнее"},
-    ]
+    const text1 = "Экспертиза отдельных конструкций, обмерные работы, определение прочности, расчет несущей способности" +
+        "и т.д."
+    const text2 = "Исследование системы отопления, вентиляции" +
+        "и кондиционирования, а также оценка энергоэффективности здания"
+    const text3 = "Цветная телевизионная съемка внутренней поверхности любых протяженных объектов" +
+        "до 250 метров"
+    const text4 = "Построение чертежей при помощи лазера, который с точностью до микрона определяет расположение объектов в пространстве"
 
-    const child   = { width: `30em`, height: `100%`}
-    const parent  = { width: `60em`, height: `100%`}
+    const cardsList = [
+        {id: v1(), image: project1, title: "Общее обследование", text: text1},
+        {id: v1(), image: project2, title: "Тепловизионная съемка", text: text2},
+        {id: v1(), image: project3, title: "Телеинспекция инженерных сетей", text: text3},
+        {id: v1(), image: project4, title: "Тахеометрическая съемка", text: text4},
+        {id: v1(), image: project1, title: "Общее обследование", text: text1},
+        {id: v1(), image: project2, title: "Тепловизионная съемка", text: text2},
+        {id: v1(), image: project3, title: "Телеинспекция инженерных сетей", text: text3},
+        {id: v1(), image: project4, title: "Тахеометрическая съемка", text: text4},
+        {id: v1(), image: project1, title: "Общее обследование", text: text1},
+        {id: v1(), image: project2, title: "Тепловизионная съемка", text: text2},
+        {id: v1(), image: project3, title: "Телеинспекция инженерных сетей", text: text3},
+        {id: v1(), image: project4, title: "Тахеометрическая съемка", text: text4},
+    ]
 
     return (
         <div className={moduleClasses["services"]}>
             <div className={globalClasses["container"]}>
-                <div className={moduleClasses["services__content"]}>
-                    <h1 className={moduleClasses["services__title"]}>Услуги</h1>
-                    <p className={moduleClasses["services__text"]}>Выполняем как комплексное обследование, так и отдельные виды работ</p>
-                </div>
-              <div className={moduleClasses["services__cards"]}>
-
-                        {cardsList.map(card =>(
-                            <Card
-                                key={card.id}
-                                itemId={card.id}
-                                title={card.title}
-                                text={card.text}
-                                photo={card.image}
-                                subtext={card.subtext}
-                            />
-                        ))}
-
+                <h1 className={moduleClasses["services__title"]}>Услуги</h1>
+                <p className={moduleClasses["services__text"]}>Выполняем как комплексное обследование, так и отдельные
+                    виды работ</p>
+                <div className={moduleClasses["services__cards"]}>
+                    {cardsList.map(card => (
+                        <Card
+                            key={card.id}
+                            itemId={card.id}
+                            title={card.title}
+                            text={card.text}
+                            photo={card.image}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
     )
 }
 
-function onWheel(apiObj, ev){
+function onWheel(apiObj, ev) {
     const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
 
     if (isThouchpad) {
