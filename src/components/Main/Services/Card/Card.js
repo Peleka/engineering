c17a76abcfbae8fb668bf4fe0418d2d5b355c53f
Peleka <ego.iriska@gmail.com>
2022-02-18 17:59:32 +0300

    refactoring mobile Services

--- src/components/Main/Services/Card/Card.js
+++ src/components/Main/Services/Card/Card.js
@@ -5,8 +5,8 @@ export const Card = ({photo, title, text}) => {
     return (
         <div className={moduleClasses["card"]}>
             <img className={moduleClasses["card__image"]} src={photo} alt={"photo"}/>
-            <h2 className={moduleClasses["card__title"]}>{title}</h2>
-            <p className={moduleClasses["card__text"]}>{text}</p>
+            <h2 className={moduleClasses["card__text"]}>{title}</h2>
+            <p className={`${moduleClasses["card__text"]} ${moduleClasses["card__text--small"]}`}>{text}</p>
             <a className={moduleClasses["card__link"]}>Подробнее</a>
         </div>
     )
